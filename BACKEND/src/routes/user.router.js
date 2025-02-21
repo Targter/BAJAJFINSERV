ee0cdb419a3d369e40506157ae6abe1840ccc4c5
AbhayBansal <126644386+Targter@users.noreply.github.com>
2025-02-21 18:03:22 +0530

    Update user.router.js

--- BACKEND/src/routes/user.router.js
+++ BACKEND/src/routes/user.router.js
@@ -4,5 +4,5 @@ const router = Router();
 
 router.route("/process").post(userIdUpload);
 router.route("/process").get(getResponse);
-router.route("/").get(getResponse);
+// router.route("/").get(getResponse);
 export default router;
