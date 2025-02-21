3379421cedfe72d194ab42610414dc75f29f951a
AbhayBansal <126644386+Targter@users.noreply.github.com>
2025-02-21 18:08:21 +0530

    Update index.js

--- BACKEND/src/index.js
+++ BACKEND/src/index.js
@@ -2,7 +2,7 @@ import app from "./app.js";
 import dotenv from "dotenv";
 
 dotenv.config({ path: "./.env" });
-try {
+
   const PORT = process.env.PORT || 8080;
   app.get("/",(req,res)=>{
     res.json({message:"Abhay_22BCS15306 SERVER IS RUNNING"})
@@ -10,6 +10,3 @@ try {
   app.listen(PORT, () => {
     console.log(`Server is running at port: ${PORT}`);
   });
-} catch (error) {
-  console.error("Error starting the server:", error);
-}
